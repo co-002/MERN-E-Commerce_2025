6a7cf923efa096c994507711905bda0ab443a55e
co-002 <aniketypandit99@gmail.com>
2025-02-27 17:56:01 +0530

    changes in api/

--- frontend/src/context/AppState.jsx
+++ frontend/src/context/AppState.jsx
@@ -5,7 +5,7 @@ import { ToastContainer, toast, Bounce } from "react-toastify";
 
 function AppState(props) {
   // const url = "http://localhost:3000/api";
-  const url = "https://myecommerce-2025-backend.onrender.com";
+  const url = "https://myecommerce-2025-backend.onrender.com/api";
   const [products, setProducts] = useState([]);
   const [token, setToken] = useState("");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
